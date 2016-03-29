4cb78821d9ec63f7957f82c66fcd94089f623b94
Nigel Hardy <nwh@aber.ac.uk>
2016-03-29 22:03:15 +0100

    backup

---
+++ Beginning New Testament/Assignments/Q1/Galationas 113-17.mm
@@ -0,0 +1,129 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1459108503408" ID="ID_1027656023" MODIFIED="1459269819825" TEXT="Galatians 1:13-17">
+<node CREATED="1459108548310" ID="ID_1995742439" MODIFIED="1459108550306" POSITION="right" TEXT="13">
+<node CREATED="1459152960602" ID="ID_1063097566" MODIFIED="1459152989474" TEXT="Who  were the &quot;you&quot;">
+<node CREATED="1459153435917" ID="ID_1275733232" MODIFIED="1459153442068" TEXT="North or south?">
+<node CREATED="1459153456458" ID="ID_1479640918" MODIFIED="1459153464585" TEXT="What does it mean for the type of people?"/>
+</node>
+</node>
+<node CREATED="1459153003124" ID="ID_968643309" MODIFIED="1459153006422" TEXT="Who was the &quot;I&quot;"/>
+<node CREATED="1459108554514" ID="ID_1885560840" MODIFIED="1459108559749" TEXT="How had they heard?"/>
+<node CREATED="1459108567933" ID="ID_1971917456" MODIFIED="1459108569668" TEXT="What was his former life?">
+<node CREATED="1459110210038" ID="ID_710887765" MODIFIED="1459151215235" TEXT="How do we know about Gamelial and being member of Sanhedrin?"/>
+</node>
+<node CREATED="1459108578064" ID="ID_314127211" MODIFIED="1459108578064" TEXT="How did he persecute?"/>
+</node>
+<node CREATED="1459108624621" ID="ID_1099801573" MODIFIED="1459108643765" POSITION="right" TEXT="14">
+<node CREATED="1459108634262" ID="ID_1651971766" MODIFIED="1459108649088" TEXT="How was he advanced?"/>
+<node CREATED="1459108656012" ID="ID_711634293" MODIFIED="1459108657090" TEXT="How was he zealous?"/>
+<node CREATED="1459108663589" ID="ID_1568561478" MODIFIED="1459108664511" TEXT="What were the traditions?"/>
+</node>
+<node CREATED="1459108671639" ID="ID_1113612111" MODIFIED="1459108673489" POSITION="right" TEXT="15">
+<node CREATED="1459108674211" ID="ID_782193324" MODIFIED="1459108678558" TEXT="Who is &#x201c;he&#x201d;?"/>
+<node CREATED="1459108684988" ID="ID_966712315" MODIFIED="1459108686248" TEXT="How was he set apart?"/>
+<node CREATED="1459108692281" ID="ID_823457549" MODIFIED="1459108693280" TEXT="How was he called?"/>
+<node CREATED="1459108700795" ID="ID_1684110798" MODIFIED="1459108702076" TEXT="What does grace mean?">
+<node CREATED="1459109255546" ID="ID_1657794034" MODIFIED="1459109440358" TEXT="Phillips p49 parag 2."/>
+</node>
+</node>
+<node CREATED="1459108703695" ID="ID_65661088" MODIFIED="1459108705649" POSITION="right" TEXT="16">
+<node CREATED="1459108713706" ID="ID_1554725092" MODIFIED="1459108722248" TEXT="How was the Son revealed?"/>
+<node CREATED="1459109445357" ID="ID_977712659" MODIFIED="1459109452888" TEXT="why &quot;in me&quot;?"/>
+<node CREATED="1459108730512" ID="ID_42500079" MODIFIED="1459108731730" TEXT="How was it that he was to preach among the Gentiles?"/>
+<node CREATED="1459108738255" ID="ID_143583899" MODIFIED="1459108739193" TEXT="Why did he not consult?"/>
+<node CREATED="1459108746496" ID="ID_833696197" MODIFIED="1459108747527" TEXT="Who might he have consulted with?"/>
+</node>
+<node CREATED="1459108750394" ID="ID_908156974" MODIFIED="1459108751769" POSITION="right" TEXT="17">
+<node CREATED="1459108760864" ID="ID_509426721" MODIFIED="1459108769634" TEXT="Why did he not go to the apostles?"/>
+<node CREATED="1459108777461" ID="ID_889194675" MODIFIED="1459108778961" TEXT="Where is Arabia; why there; other evidence for his going there; how long for;"/>
+<node CREATED="1459108787636" ID="ID_1878687185" MODIFIED="1459108788761" TEXT="When back to Damascus; why there?"/>
+</node>
+<node CREATED="1459108949039" ID="ID_1182956007" MODIFIED="1459108949039" POSITION="left" TEXT="">
+<node CREATED="1459108968005" ID="ID_1107380225" MODIFIED="1459258847420" TEXT="13 For you have heard of my former life in Judaism, how I persecuted the church of God violently and tried to destroy it.&#xa;14 And I was advancing in Judaism beyond many of my own age among my people, so extremely zealous was I for the traditions of my fathers.&#xa;15 But when he who had set me apart before I was born, and who called me by his grace,&#xa;16 was pleased to reveal his Son to me, in order that I might preach him among the Gentiles, I did not immediately consult with anyone;&#xa;17 nor did I go up to Jerusalem to those who were apostles before me, but I went away into Arabia, and returned again to Damascus.">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+<node CREATED="1459109849766" ID="ID_669979080" MODIFIED="1459110077063" POSITION="left" TEXT="separated, saved, sanctified, sent (Philips p51, parag 2"/>
+<node CREATED="1459110079138" ID="ID_1343139331" MODIFIED="1459152793367" POSITION="left" TEXT="reminds Galatians of his conversion, &amp; obliquely, theirs (Phillips p52, parag 2"/>
+<node CREATED="1459152748275" FOLDED="true" ID="ID_23590728" MODIFIED="1459197706558" POSITION="left" TEXT="Hooker">
+<node CREATED="1459180679988" ID="ID_1614260620" MODIFIED="1459180698395" TEXT="Born &amp; brough up a Jew">
+<node CREATED="1459180701259" ID="ID_187871910" MODIFIED="1459180715560" TEXT="Circumcised (law)"/>
+<node CREATED="1459180716223" ID="ID_1196969263" MODIFIED="1459180722677" TEXT="Benjamin"/>
+<node CREATED="1459180723373" ID="ID_1018239085" MODIFIED="1459180731064" TEXT="Pharasee in observance of the law"/>
+</node>
+<node CREATED="1459180738368" ID="ID_1409097112" MODIFIED="1459180740668" TEXT="Believed:">
+<node CREATED="1459180744425" ID="ID_577202665" MODIFIED="1459180752648" TEXT="1 God">
+<node CREATED="1459180755042" ID="ID_1222486583" MODIFIED="1459180757867" TEXT="Creator"/>
+<node CREATED="1459180758700" ID="ID_1398465465" MODIFIED="1459180767937" TEXT="Chose israelites"/>
+<node CREATED="1459180779106" ID="ID_461792095" MODIFIED="1459180784925" TEXT="Revieled to Moses"/>
+<node CREATED="1459180793930" ID="ID_572755306" MODIFIED="1459180797123" TEXT="Covenant"/>
+<node CREATED="1459180807665" ID="ID_1088058817" MODIFIED="1459180815411" TEXT="Righteous"/>
+<node CREATED="1459180886291" ID="ID_863793193" MODIFIED="1459180897230" TEXT="Guarded and guided his people">
+<node CREATED="1459180898024" ID="ID_380530254" MODIFIED="1459180908438" TEXT="they rebelled  and sinned"/>
+<node CREATED="1459180924319" ID="ID_339875082" MODIFIED="1459180957160" TEXT="he remained faithful"/>
+</node>
+</node>
+<node CREATED="1459180972462" ID="ID_709244068" MODIFIED="1459180979816" TEXT="end in the future">
+<node CREATED="1459180986852" ID="ID_501278709" MODIFIED="1459180992993" TEXT="God re-established rule">
+<node CREATED="1459181002325" ID="ID_1858917980" MODIFIED="1459181011141" TEXT="rebellion crushed "/>
+<node CREATED="1459181012163" ID="ID_1385091129" MODIFIED="1459181020763" TEXT="All creation obedient"/>
+</node>
+<node CREATED="1459181022951" ID="ID_1670383261" MODIFIED="1459181041584" TEXT="paradies restored">
+<node CREATED="1459181044060" ID="ID_1648929678" MODIFIED="1459181048522" TEXT="new heaven &amp; new earth"/>
+</node>
+<node CREATED="1459181059622" ID="ID_1387998375" MODIFIED="1459181063276" TEXT="die before">
+<node CREATED="1459181065994" ID="ID_478315836" MODIFIED="1459181074667" TEXT="resurrection">
+<node CREATED="1459181076349" ID="ID_465172946" MODIFIED="1459181082743" TEXT="believed by some"/>
+<node CREATED="1459181083851" ID="ID_1294120380" MODIFIED="1459181091770" TEXT="inc pharisees"/>
+</node>
+</node>
+<node CREATED="1459181107787" ID="ID_1468738866" MODIFIED="1459181130519" TEXT="disobedient punished">
+<node CREATED="1459181132164" ID="ID_493488448" MODIFIED="1459181140679" TEXT="within Israel"/>
+<node CREATED="1459181141281" ID="ID_1346197955" MODIFIED="1459181143612" TEXT="and Gentiles"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1459269821543" FOLDED="true" ID="ID_463117580" MODIFIED="1459274075916" POSITION="left" TEXT="Boxall">
+<node CREATED="1459269829311" ID="ID_439329642" MODIFIED="1459269857173" TEXT="astinished that the gospal is perverted so quickly"/>
+<node CREATED="1459269858466" ID="ID_917264968" MODIFIED="1459269878901" TEXT="can&apos;t be members if they do not adopt Jewish practices">
+<node CREATED="1459269880629" ID="ID_88578024" MODIFIED="1459269884799" TEXT="circumcision"/>
+</node>
+<node CREATED="1459269903677" ID="ID_1026980433" MODIFIED="1459269915452" TEXT="all stand equal in Christ">
+<node CREATED="1459269925091" ID="ID_164419275" MODIFIED="1459269930896" TEXT="Gentiles are not 2nd class"/>
+</node>
+<node CREATED="1459269945945" ID="ID_662565461" MODIFIED="1459269964008" TEXT="on what grounds do we decide Christ&apos;s will?"/>
+<node CREATED="1459269986390" ID="ID_439108653" MODIFIED="1459269993176" TEXT="Opponents have high gound?">
+<node CREATED="1459269993804" ID="ID_776649785" MODIFIED="1459269996898" TEXT="scripture"/>
+<node CREATED="1459269997444" ID="ID_1744714727" MODIFIED="1459270022114" TEXT="tradition (J.C. did not say no circumcision)"/>
+<node CREATED="1459270023680" ID="ID_1343003400" MODIFIED="1459270032396" TEXT="apostles in Jerusalem"/>
+</node>
+<node CREATED="1459270041986" ID="ID_1949610824" MODIFIED="1459270056440" TEXT="charge that h is the innovator">
+<node CREATED="1459270065946" ID="ID_1806996511" MODIFIED="1459270093146" TEXT="going against devine revelation and community practice"/>
+</node>
+<node CREATED="1459270096982" ID="ID_902960309" MODIFIED="1459270104431" TEXT="point by point">
+<node CREATED="1459270112649" ID="ID_267700690" MODIFIED="1459270420146" TEXT="based on damascus road (Gal 1)">
+<node CREATED="1459270123885" ID="ID_633237820" MODIFIED="1459271940688" TEXT="teaching is result of divine revelation"/>
+<node CREATED="1459270136881" ID="ID_869386913" MODIFIED="1459270146405" TEXT="not due to human intermediary"/>
+<node CREATED="1459270165631" ID="ID_1682345143" MODIFIED="1459271859296" TEXT="sidesteps JC not advocating abandoning circumcision"/>
+</node>
+<node CREATED="1459270205451" ID="ID_1125761760" MODIFIED="1459271842696" TEXT="distances himself from original apostles but says the accepted non-circumcision (Gal 2)"/>
+<node CREATED="1459270249593" ID="ID_402601011" MODIFIED="1459270454944" TEXT="says Abraham storysupports position (Gal 3,4)"/>
+<node CREATED="1459270272645" ID="ID_326031517" MODIFIED="1459270305690" TEXT="Jews expected Spirit">
+<node CREATED="1459270309292" ID="ID_1164031854" MODIFIED="1459270463201" TEXT="recepion without circumcision or works of the law rendered them superfluous (gal 3.2)"/>
+</node>
+</node>
+<node CREATED="1459270352895" ID="ID_792951782" MODIFIED="1459270362312" TEXT="what part should polemics play?">
+<node CREATED="1459270382250" ID="ID_1223081063" MODIFIED="1459270397303" TEXT="(strong verbal or written attack)"/>
+</node>
+</node>
+<node CREATED="1459274079415" ID="ID_806373736" MODIFIED="1459274107367" POSITION="left" TEXT="Phillips">
+<node CREATED="1459274121865" ID="ID_1965588233" MODIFIED="1459274125285" TEXT="spiritual history">
+<node CREATED="1459274127656" ID="ID_679063337" MODIFIED="1459274131243" TEXT="separated before birth"/>
+<node CREATED="1459274212412" ID="ID_1245562536" MODIFIED="1459274249032" TEXT="separated at conversion (Acts 9:15-16 - Ananias)"/>
+<node CREATED="1459274295396" ID="ID_959482935" MODIFIED="1459274325518" TEXT="separated at ordination (Acts 13:2 with Barnabus)"/>
+</node>
+</node>
+</node>
+</map>
